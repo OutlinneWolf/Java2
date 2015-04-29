a8dbc75ff5002fec6daf292cfafc30e7725447ec
OutlinneWolf <riki1998200926@gmail.com>
2015-04-29 08:49:15 +0100

    init commit

---
+++ src/Pessoas/Program.java
@@ -0,0 +1,22 @@
+package Pessoas;
+
+public class Program {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		Pessoa p1 = new Pessoa("Antonio");
+		Pessoa p2 = new Pessoa("Bruno", "Alves");
+		
+	//	System.out.println(p1.getName());
+	//	System.out.println(p2.getName()+ " " + p2.getApelido());
+		
+		Utils.teste =10;
+		
+		System.out.println(Utils.teste);
+	}
+
+}
---
+++ src/Pessoas/Utils.java
@@ -0,0 +1,18 @@
+package Pessoas;
+
+public class Utils {
+	
+	public static int teste;
+	
+	private Utils()
+	{
+		
+	}
+	
+	public static void dizOla()
+	{
+		System.out.println("Ola");
+	}
+	
+	
+}
